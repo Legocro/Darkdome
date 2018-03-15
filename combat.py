474d5b6e3bc3433e03a6e3fbfe7a39b0373c3716
Loki <ivan.legovic@gmail.com>
2018-03-15 22:58:50 +0100

    cb formula?
    TODO:Move all cb related scripts/ functions here

---
+++ combat.py
@@ -0,0 +1,14 @@
+#Functions for combat and experience
+
+from classes import Character
+from classes import Human
+from random import *
+
+def cb:
+	if self.health > 0:
+		pdmg = (self.strength * (randint(7, 12)) * 10
+
+def cbxp:
+	cbxp = (pdmg * 40) / 120
+
+#Move all cb related functions here
